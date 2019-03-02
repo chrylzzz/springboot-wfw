62066e7544efee358288b1be7a2ef638154ae60a
cyl5yy412 <cyl5yy412@gmail.com>
2019-03-02 14:11:29 +0800

    init

--- eureka-client/src/main/java/com/lnsoft/controller/ConfigController.java
+++ eureka-client/src/main/java/com/lnsoft/controller/ConfigController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 /**
+ * 暂未实现：
  * 测试从配置中心服务端（最终从git仓库）读取配置
  * Created By Chr on 2019/2/28/0028.
  */
@@ -16,35 +17,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RefreshScope
 public class ConfigController {
 
-    private static Logger logger= LoggerFactory.getLogger(ConfigController.class);
+    private static Logger logger = LoggerFactory.getLogger(ConfigController.class);
 
     @Value("${changsha.pilot.name}")
     String message;
 
     @RequestMapping("/getConfig")
-    public String getPropertyFromConfigServer(){
+    public String getPropertyFromConfigServer() {
         String msg = "message from config server : " + message;
         logger.info(msg);
         return msg;
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 }
