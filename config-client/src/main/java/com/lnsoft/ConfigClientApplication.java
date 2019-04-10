664c6e5c6d0ba5ca01cdb4e3d17a6c887bee1bb1
cyl5yy412 <cyl5yy412@gmail.com>
2019-04-10 09:31:35 +0800

    init

--- config-client/src/main/java/com/lnsoft/ConfigClientApplication.java
+++ config-client/src/main/java/com/lnsoft/ConfigClientApplication.java
@@ -18,8 +18,11 @@ public class ConfigClientApplication {
 
 
 //	打开网址访问：http://localhost:8881/hi，网页显示：
+
 //	foo version 3
+
 //	这就说明，config-client从config-server获取了foo的属性，
+
 // 	而config-server是从git仓库读取的,如图：图片在resources目录
 
 	@Value("${foo}")
